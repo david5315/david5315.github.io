ab05b03ce028c2de4f0816dad01fd48253b5c2d2
david5315 <53157652@qq.com>
2026-05-16 23:07:21 +0800

    Delete exe directory

--- exe/生成exe文件打包多个文件夹 app.spec.cmd
+++
@@ -1,5 +0,0 @@
- 
-
-D:\Users\Administrator\AppData\Local\Programs\Python\Python313\Scripts\pip install pyinstaller 
-D:\Users\Administrator\AppData\Local\Programs\Python\Python313\Scripts\pyinstaller app.spec
-pause
